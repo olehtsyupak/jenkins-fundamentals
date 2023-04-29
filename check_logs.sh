b6336f6c5b491ecf654df5551cff8c37c10f943e
Oleh Tsiupak <tsyupak98@gmail.com>
2023-04-29 11:14:14 +0000

    added apache installation

--- check_logs.sh
+++ check_logs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! dpkg -l | grep -q apache2; then
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
+fi
+
 log_file="/var/log/apache2/access.log"
 
 errors=$(grep -E ' [45][0-9]{2} ' $log_file)
